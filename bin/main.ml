64c05297e2922c21eabe69f1b5ee5bf8cd9b4836
José Luis Romero <jose.romero@ing.uchile.cl>
2023-04-24 11:57:33 -0400

    Adds CLI

--- bin/main.ml
+++ bin/main.ml
@@ -1,44 +1,62 @@
-open Middle_end
-open Common
-open Syntax
-open Parsing
-open Format
+open Cmdliner
+open! Middle_end
+open! Common
+open! Syntax
+open! Format
+open! Driver
 
-let compile (e : Ast.e) =
-  printf "%a\n\n%!" Ast.pp_e e;
-  printf "Uniquifying\n%!";
-  let e = Ast.uniquify e in
-  printf "%a\n\n%!" Ast.pp_e e;
-  printf "Transforming to ANF\n%!";
-  let anf = Anf.trans e in
-  printf "%a\n\n%!" Anf.pp_expr anf;
-  printf "Beta reducing\n%!";
-  let anf = Anf.beta_reduce anf in
-  printf "%a\n\n%!" Anf.pp_expr anf;
-  printf "Removing unused let bindings\n%!";
-  let anf = Anf.remove_unused_let_bindings anf in
-  printf "%a\n\n%!" Anf.pp_expr anf;
-  printf "Converting closures\n%!";
-  let program = Closure.closure anf in
-  printf "%a\n\n%!" Closure.pp_program program;
-  printf "Converting to llvm IR\n%!";
-  Codegen.Module.init "main";
-  let funcs = Codegen.codegen_program program in
-  let the_module = Codegen.Module.get () in
-  Llvm.dump_module the_module;
-  funcs
+type verb = Verbose | Quiet
 
-let _ =
-  try
-    let ast =
-      match Driver.parse_stdin () with
-      | Some v -> v
-      | None ->
-          eprintf "Input is empty";
-          exit 1
-    in
-    let _ = compile ast in
-    ()
-  with Error.Error e ->
-    Format.eprintf "%a\n" Error.pp_loc e;
-    exit 1
+let input_file_arg =
+  let doc = "The input file to be compiled" in
+  let docv = "INPUT_FILE" in
+  Arg.(required & pos 0 (some string) None & info [] ~docv ~doc)
+
+let output_file_arg =
+  let doc =
+    "The output file for the compiled LLVM IR (default: input file with '.ll' \
+     extension)"
+  in
+  let docv = "OUTPUT_FILE" in
+  Arg.(value & opt (some string) None & info [ "o"; "output-file" ] ~docv ~doc)
+
+let verb_t =
+  let quiet =
+    let doc = "Don't print the steps in the compilation pipeline." in
+    (Quiet, Arg.info [ "q"; "quiet"; "silent" ] ~doc)
+  in
+  let verbose =
+    let doc = "Print every step in the compilation pipeline." in
+    (Verbose, Arg.info [ "v"; "verbose" ] ~doc)
+  in
+  Arg.(last & vflag_all [ Quiet ] [ quiet; verbose ])
+
+let compile input_file output_file_opt verb =
+  let output_file =
+    match output_file_opt with
+    | Some f -> f
+    | None -> Filename.remove_extension input_file ^ ".ll"
+  in
+  let verbose = match verb with Verbose -> true | Quiet -> false in
+  Utils.compile_file input_file output_file ~verbose
+
+let compiler_t =
+  Term.(const compile $ input_file_arg $ output_file_arg $ verb_t)
+
+let compiler_cmd =
+  let doc = "A simple compiler" in
+  let man =
+    [
+      `S "DESCRIPTION";
+      `P
+        "Compiles the program in the input file to LLVM IR and writes the \
+         result to the output file.";
+      `S "EXAMPLES";
+      `P "To compile a file named 'program.txt' and output to 'program.ll':";
+      `P "$(b,compiler) program.txt -o program.ll";
+    ]
+  in
+  let info = Cmd.info "toy_compiler" ~version:"0.0.1" ~doc ~man in
+  Cmd.v info compiler_t
+
+let () = exit (Cmd.eval compiler_cmd)
--- lib/driver/utils.ml
+++ lib/driver/utils.ml
@@ -1,27 +1,55 @@
 open Syntax
 open Middle_end
+open Common
 
-let compile_e e output_file =
+let compile_e ?(verbose = false) e output_file =
+  let log t =
+    if verbose then Format.eprintf t else Format.ifprintf Format.err_formatter t
+  in
+  log "%a\n\n%!" Ast.pp_e e;
+  log "Uniquifying\n%!";
   let e = Ast.uniquify e in
+  log "%a\n\n%!" Ast.pp_e e;
+  log "Transforming to ANF\n%!";
   let anf = Anf.trans e in
+  log "%a\n\n%!" Anf.pp_expr anf;
+  log "Beta reducing\n%!";
   let anf = Anf.beta_reduce anf in
+  log "%a\n\n%!" Anf.pp_expr anf;
+  log "Removing unused let bindings\n%!";
   let anf = Anf.remove_unused_let_bindings anf in
+  log "%a\n\n%!" Anf.pp_expr anf;
+  log "Converting closures\n%!";
   let program = Closure.closure anf in
+  log "%a\n\n%!" Closure.pp_program program;
+  log "Converting to llvm IR\n%!";
   let _ = Codegen.codegen_program program in
   let the_module = Codegen.Module.get () in
   Llvm.print_module output_file the_module
 
-let compile_string str output_file =
-  let e = Option.get (Parsing.Driver.parse_string str) in
-  compile_e e output_file
+let try_compile ?(verbose = false) parse_func output_file =
+  try
+    Codegen.Module.init "main";
+    let e = parse_func () in
+    compile_e e output_file ~verbose
+  with Error.Error e ->
+    Format.eprintf "%a\n" Error.pp_loc e;
+    exit 1
 
-let compile_file input_file output_file =
-  let e = Option.get (Parsing.Driver.parse_file input_file) in
-  compile_e e output_file
+let compile_string ?(verbose = false) str output_file =
+  try_compile ~verbose
+    (fun () -> Option.get (Parsing.Driver.parse_string str))
+    output_file
 
-let compile_stdin output_file =
-  let e = Option.get (Parsing.Driver.parse_stdin ()) in
-  compile_e e output_file
+let compile_file ?(verbose = false) input_file output_file =
+  try_compile ~verbose
+    (fun () -> Option.get (Parsing.Driver.parse_file input_file))
+    output_file
+
+let compile_stdin ?(verbose = false) output_file =
+  try_compile ~verbose
+    (fun () -> Option.get (Parsing.Driver.parse_stdin ()))
+    output_file
 
 let run_file (filename : string) =
   let cmd = Filename.quote_command "lli" [ filename ] in
